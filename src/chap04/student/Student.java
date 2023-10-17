2742944b467526c71de2c441e7a9957ef789dc88
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-10-17 17:03:00 +0900

    [Java] 클래스와 객체-1

---
+++ src/chap04/student/Student.java
@@ -0,0 +1,23 @@
+package src.chap04.student;
+
+public class Student {
+    String studentNumber;
+    String department;
+    String name;
+    String phoneNumber;
+
+
+    public Student(String studentNumber, String department, String name, String phoneNumber) {
+        this.studentNumber = studentNumber;
+        this.department = department;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    void print() {
+        System.out.println("학번 : " + this.studentNumber);
+        System.out.println("학과 : " + this.department);
+        System.out.println("이름 : " + this.name);
+        System.out.println("전화번호 : " + this.phoneNumber + "\n");
+    }
+}
