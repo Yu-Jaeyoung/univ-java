8cdb108faa2c7d24c9ed78fcad0c02628b99b1ed
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-11-21 15:58:22 +0900

    [Java] 패키지와 API

---
+++ src/chap06/ImportExample.java
@@ -0,0 +1,9 @@
+package src.chap06;
+
+// import java.util.*; 컴파일 시간에 차이가 존제
+import java.util.Scanner;
+
+public class ImportExample {
+    Scanner scanner = new Scanner(System.in);
+    // java.util.Scanner scanner = new java.util.Scanner(System.in);
+}
